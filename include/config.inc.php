27d9dc60ab2d550316280c9a94dd22f8446139e6
Francesco <77879644+VampaFrancesco@users.noreply.github.com>
2025-07-28 15:47:37 +0200

    setup directory

---
+++ include/config.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+$config['skin'] = "liberty_market";
+$config['dashboard'] = "admin";
+
+$config['dbms']['localhost']['user'] = "root";
+$config['dbms']['localhost']['passwd'] = "";
+$config['dbms']['localhost']['host'] = "localhost";
+$config['dbms']['localhost']['dbname'] = "nomesito";
+
+$config['tinymce']['apikey'] = "lfx5nv6aaiughydede4ntewpxrrz54tqwrkqu8tk87kzcp0h";
+
+$HOME_ELEMENTS_PATH = "home/";
+$EXPLORE_ELEMENTS_PATH = "explore/";
+
+?>
---
+++ pages/signup.php
@@ -0,0 +1,5 @@
+<?php
+
+
+require "include/config.inc.php";
+require "include/template2.inc.php";
